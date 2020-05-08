dd1ec75a4244815072d284ed450f1067e7308fd4
antonkudrin <atlet-2@mail.ru>
2020-05-08 17:39:39 +0500

    message

--- formdata.php
+++ formdata.php
@@ -1,9 +1,9 @@
 <?php
 $result = mail ("atlet-2@mail.ru","Получен Комментарий с сайта","
     \nC сайта Книжный магазин был получен Комментарий:
-	\nИмя: $_POST[inputText3]
-	\nПочта: $_POST[inputEmail3]
-	\nСообщение: $_POST[exampleFormControlTextarea1]");
+	\nИмя: $_POST['inputText3']
+	\nПочта: $_POST['inputEmail3']
+	\nСообщение: $_POST['exampleFormControlTextarea1']");
 if ($result) {
 	echo "<p>Сообщение отправлено успешно!</p>";
 }
